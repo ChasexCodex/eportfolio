491c9748a2588bfad8ba0ba7d68e714d68fd0c86
chasexcodex <chase.x.codex@gmail.com>
2022-11-13 18:14:57 +0300

    fix pages compile

---
+++ pages/work/english/index.tsx
@@ -0,0 +1,14 @@
+import Page from '../../../components/Page'
+import MainHeader from '../../../components/MainHeader'
+import NavBar from '../../../components/NavBar'
+import MainFooter from '../../../components/MainFooter'
+
+export default function English() {
+  return (
+    <Page>
+      <NavBar/>
+      <MainHeader/>
+      <MainFooter/>
+    </Page>
+  )
+}
